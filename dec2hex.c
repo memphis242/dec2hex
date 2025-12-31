90bcdb838d1fb10c0feabe32c6a9f781b33afaf0
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-12-30 22:19:50 -0600

    :building_construction: Quick draft of `dec2hex` program tool

---
+++ dec2hex.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char * argv[])
+{
+   if ( argc != 2 )
+   {
+      fprintf( stderr, "Invalid number of args: %d\n", argc );
+      return 1;
+   }
+
+   char * endptr = memchr(argv[1], '\0', 15);
+   if ( endptr == nullptr )
+   {
+      fprintf( stderr, "Number argument too long!\n" );
+   }
+
+   errno = 0;
+   unsigned long int num = strtol(argv[1], &endptr, 10);
+   if ( errno != 0 )
+   {
+      fprintf( stderr,
+               "Unable to strtol()\n"
+               "returned: %ld, errno: %s (%d): %s\n",
+               num, strerrorname_np(errno), errno, strerror(errno) );
+      return 2;
+   }
+   else if ( endptr == nullptr )
+   {
+      fprintf( stderr, "Somehow endptr is nullptr from strtol()...!\n" );
+      return 3;
+   }
+   else if ( *endptr != '\0' )
+   {
+      fprintf( stderr,
+               "Invalid character at: argv[1][%td] : %c\n",
+               endptr - argv[1], *endptr );
+      return 4;
+   }
+
+   printf("0x%0lX\n", num);
+
+   return 0;
+}
